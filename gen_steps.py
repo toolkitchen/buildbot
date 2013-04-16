b91f69a5ab8002069d8b986bec176b2a5366f08f
Yvonne Yip <ykyyip@google.com>
2013-04-16 16:44:11 -0700

    run a shell script for test to print annotations

--- gen_steps.py
+++ gen_steps.py
@@ -10,6 +10,6 @@ def GetSteps(api, _factory_properties, build_properties):
                ['npm', 'install'],
                cwd=api.checkout_path()),
     steps.step('test',
-               ['xvfb-run', 'grunt', 'test-buildbot'],
+               ['run_tests.sh'],
                cwd=api.checkout_path()),
   ]
