4e0d17953da13090a55cbe747a93458cbd83b6fc
Yvonne Yip <ykyyip@google.com>
2013-04-17 11:54:15 -0700

    debug mactester

--- gen_steps.py
+++ gen_steps.py
@@ -6,6 +6,9 @@ https://chromium.googlesource.com/chromium/tools/build/"""
 def GetSteps(api, _factory_properties, build_properties):
   steps = api.Steps(build_properties)
   return [
+    steps.step('print-env',
+               ['buildbot/print_env.sh'],
+               cwd=api.checkout_path()),
     steps.step('update-install',
                ['npm', 'install'],
                cwd=api.checkout_path()),
---
+++ print_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+npm config get tmp
+echo "TMP $TMP"
+echo "TMPDIR $TMPDIR"
