6c3f11fdfc45e1aed5b459b96bea11e83dd75484
Aaron Gable <agable@google.com>
2013-04-29 15:28:00 -0700

    Changing steps to skip xvfb on mac and win.

--- gen_steps.py
+++ gen_steps.py
@@ -3,8 +3,13 @@
 For more information, see scripts/slave/annotated_run.py in
 https://chromium.googlesource.com/chromium/tools/build/"""
 
+import sys
+
 def GetSteps(api, _factory_properties, build_properties):
   steps = api.Steps(build_properties)
+  test_cmd = ['grunt', 'test-buildbot']
+  if sys.platform.startswith('linux2'):
+    test_cmd = ['xvfb-run'] + test_cmd
   return [
     steps.step('mktmp',
                ['mkdir', '.tmp'],
@@ -13,6 +18,6 @@ def GetSteps(api, _factory_properties, build_properties):
                ['npm', 'install', '--tmp', '.tmp'],
                cwd=api.checkout_path()),
     steps.step('test',
-               ['buildbot/run_tests.sh'],
+               test_cmd,
                cwd=api.checkout_path()),
   ]
